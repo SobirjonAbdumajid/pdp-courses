a0234a6f5ae8fdcd55b37892c3ebf5519c901aa2
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-25 11:22:38 +0500

    tasks

--- 01-pdp-database-cours/12-subquery-any-all.sql
+++ 01-pdp-database-cours/12-subquery-any-all.sql
@@ -1,10 +1,20 @@
 select * from city;
 select * from country;
 
-select * from city;
+# 1
+select name, Population
+from country
+where population=(select max(Population) from country);
 
+# 2
+select name, population
+from country
+where population>(select avg(population) from country);
 
-
+# 3
+select name
+from country
+where population<(select avg(population) from country);
 
 
 
