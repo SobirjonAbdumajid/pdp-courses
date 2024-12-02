2792edcb6319e3c621810b05a98c95bf8d247888
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-02 12:58:00 +0500

    tasks

--- 01-pdp-database-cours/14-clean-data.sql
+++ 01-pdp-database-cours/14-clean-data.sql
@@ -14,3 +14,20 @@ select phone, concat(substring(phone,1,3),'-',substring(phone, 4,3),'-', substri
 update cleandata
 set phone=concat(substring(phone,1,3),'-',substring(phone, 4,3),'-', substring(phone,7));
 
+select favorite_color, upper(favorite_color) from cleandata;
+
+update cleandata
+set favorite_color=upper(favorite_color);
+
+select `Are you over 18?`, case
+when `Are you over 18?`='Yes' then 'Y'
+when `Are you over 18?`='No' then 'N'
+else `Are you over 18?` end from cleandata;
+
+update cleandata
+set `Are you over 18?`=case
+when `Are you over 18?`='Yes' then 'Y'
+when `Are you over 18?`='No' then 'N'
+else `Are you over 18?` end;
+
+
